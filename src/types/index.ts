f6bb84bcb2a9fab024ff9160a509061d0a0f04f3
avganka <karambasterg@gmail.com>
2023-06-25 22:34:48 +0300

    add api types

---
+++ src/types/index.ts
@@ -0,0 +1,71 @@
+export type Id = number;
+
+export interface Post {
+  userId: Id;
+  id: Id;
+  title: string;
+  body: string;
+}
+
+export interface PostWithComments extends Post {
+  comments: Comment[];
+}
+
+export interface Album {
+  userId: Id;
+  id: Id;
+  title: string;
+}
+
+export interface Comment {
+  postId: Id;
+  id: Id;
+  name: string;
+  email: string;
+  body: string;
+}
+
+export interface Photo {
+  albumId: Id;
+  id: Id;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
+
+export interface Todo {
+  userId: Id;
+  id: Id;
+  title: string;
+  completed: boolean;
+}
+
+export interface User {
+  id: Id;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
