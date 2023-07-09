075544357421483e93efa9d88b159a3bd1d4b927
avganka <karambasterg@gmail.com>
2023-07-10 00:53:08 +0300

    fix api config

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,4 @@
 import axios from 'axios';
-import {Id, Post, User} from '../types';
-import {API_ROUTES} from './apiRoutes';
 
 export const api = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com/',
@@ -8,22 +6,3 @@ export const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
-export const fetcher = (url: string) => api.get(url).then((res) => res.data);
-
-export const getPosts = (page: number, limit = 10) =>
-  api.get(`${API_ROUTES.posts}/?_page=${page}?_limit=${limit}`).then((res) => res.data);
-
-export const getPostComments = (postId: Id) =>
-  api.get(`${API_ROUTES.posts}/${postId}/comments`).then((res) => res.data);
-
-export const getUserById = (userId: Id) =>
-  api.get(`${API_ROUTES.users}/${userId}`).then((res) => res.data);
-
-export const getUsers = () => api.get(`${API_ROUTES.users}`).then((res) => res.data);
-
-export const updatePost = (postId: Id, post: Post) =>
-  api.put(`${API_ROUTES.posts}/${postId}`, post).then((res) => res.data);
-
-export const updateUser = (userId: Id, user: User) =>
-  api.put(`${API_ROUTES.users}/${userId}`, user).then((res) => res.data);
--- src/api/apiRoutes.ts
+++ src/api/apiRoutes.ts
@@ -1,7 +1,41 @@
-export const API_ROUTES = {
-  posts: '/posts',
-  todos: '/todos',
-  albums: '/albums',
-  users: '/users',
-  comments: '/comments',
+import {SortType} from '../types';
+
+interface SearchParams {
+  limit?: number;
+  page?: number;
+  order?: SortType;
+  q?: string;
+}
+
+export const getUsersRoute = () => '/users';
+
+export const getPostsRoute = () => '/posts';
+
+export const getPostsWithUserRoute = (searchParams: SearchParams) => {
+  const {limit, page, order, q} = searchParams;
+  const search = new URLSearchParams();
+  search.set('_expand', 'user');
+  if (!searchParams) {
+    return search.toString();
+  }
+
+  if (limit && page) {
+    search.set('_limit', limit.toString());
+    search.set('_page', page.toString());
+  }
+
+  if (order) {
+    if (order !== 'popular') {
+      search.set('_sort', 'title,body');
+      search.set('_order', order);
+    }
+  }
+  if (q) {
+    search.set('q', q);
+  }
+
+  return `/posts?${search.toString()}`;
 };
+
+export const getPostRoute = (postId: string | number) => `/posts/${postId}`;
+export const getPostCommentsRoute = (postId: string | number) => `/posts/${postId}/comments`;
